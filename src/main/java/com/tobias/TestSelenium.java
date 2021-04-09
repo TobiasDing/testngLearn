4b01f384642a01d11c71e72efc9d187235cc9480
TobiasDing <tobias.ding@hotmail.com>
2021-04-09 22:20:07 +0800

    111

--- src/main/java/com/tobias/TestSelenium.java
+++ src/main/java/com/tobias/TestSelenium.java
@@ -1,6 +1,6 @@
 package com.tobias;
 
-import org.junit.Assert;
+import org.testng.Assert;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
@@ -14,36 +14,41 @@ import org.junit.Assert.*;
 import java.util.Set;
 
 public class TestSelenium {
+    WebDriver driver;
 
     @DataProvider(name="url")
     public Object[][] url(){
         return new Object[][] {
-                {"https://www.baidu.com"}
+                {"audi"},
+                {"bmw"}
         };
     }
     @BeforeTest
     public void before() {
         System.setProperty("webdriver.chrome.driver", "/usr/local/bin/chromedriver");
+        driver = new ChromeDriver();
+        System.out.println("Chrome Browser opened.");
 
     }
 
     @Test(dataProvider = "url")
-    public void testOpen(String url) throws InterruptedException {
-        WebDriver driver = new ChromeDriver();
-        driver.get(url);
+    public void testSearch(String kw) throws InterruptedException {
+
+        driver.get("https://www.baidu.com");
         Thread.sleep(100);
         String windowHandle = driver.getWindowHandle();
-        driver.findElement(By.id("kw")).sendKeys("audi");
+        driver.findElement(By.id("kw")).sendKeys(kw);
         driver.findElement(By.id("su")).click();
 //        Set<String> h = driver.getWindowHandles();
-        Thread.sleep(500);
+        Thread.sleep(100);
         String currentUrl = driver.getCurrentUrl();
-        Assert.assertTrue(currentUrl.contains("audi"));
-        driver.close();
+        Assert.assertTrue(currentUrl.contains("123"), String.format("Keyword:%s, searching FAILED!", kw));
+
     }
 
     @AfterTest
     public void after() {
-        System.out.println("after");
+        driver.close();
+        System.out.println("Chrome Browser closed.");
     }
 }
